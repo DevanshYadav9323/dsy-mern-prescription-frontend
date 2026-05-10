90ede4af1635702d347821dbcd12521d9ea9a3b7
Devansh Yadav <devanshyadav9323@gmail.com>
2026-05-10 10:51:36 +0530

    AppConfig --minor

--- app/containers/App/constants/config.js
+++ app/containers/App/constants/config.js
@@ -1,7 +1,6 @@
 const AppConfig = {
-    // baseUrl: 'https://api.shopkya.in',
-    // baseUrl: 'https://stagingapi.shopkya.in',
-   baseUrl: 'http://localhost:3100',
+    baseUrl: 'https://dsy-mern-prescription-backend.onrender.com',
+  //  baseUrl: 'http://localhost:3100',
  }
  
  export default AppConfig;
